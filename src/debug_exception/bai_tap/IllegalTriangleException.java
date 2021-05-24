29125025df54499e934f396a38c28c9516d78699
nguyennguyen410 <nguyen.nhn89@gmail.com>
2021-05-24 16:38:57 +0700

    Bug & Exception 1

--- src/debug_exception/bai_tap/IllegalTriangleException.java
+++ src/debug_exception/bai_tap/IllegalTriangleException.java
@@ -1,8 +1,13 @@
 package debug_exception.bai_tap;
 
-public class IllegalTriangleException extends Throwable {
-    @Override
-    public String getMessage() {
-        return "Nhap sai";
+public class IllegalTriangleException extends Exception {
+    public void illegalTriangleException(double side1, double side2, double side3) throws Exception {
+        if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+            throw new Exception("Cac canh phai lon hon 0.");
+        } else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+            throw new Exception("Tong 2 canh phai lon hon canh con lai.");
+        } else {
+            System.out.println("Perimeter is:" + (side1 + side2 + side3));
+        }
     }
 }
--- src/debug_exception/bai_tap/IllegalTriangleExceptionExample.java
+++ src/debug_exception/bai_tap/IllegalTriangleExceptionExample.java
@@ -3,23 +3,29 @@ package debug_exception.bai_tap;
 import java.util.Scanner;
 
 public class IllegalTriangleExceptionExample {
-    public static void main(String[] args) throws IllegalTriangleException {
-        double side1;
-        double side2;
-        double side3;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input side1: ");
-        side1 = input.nextDouble();
-        System.out.println("Input side2: ");
-        side2 = input.nextDouble();
-        System.out.println("Input side3: ");
-        side3 = input.nextDouble();
-        if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-            throw new IllegalTriangleException();
-        } else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
-            throw new IllegalTriangleException();
-        } else {
-            System.out.println("Perimeter is:" + (side1 + side2 + side3));
+
+    public static void main(String[] args) throws Exception {
+        double side1 = 0;
+        double side2 = 0;
+        double side3 = 0;
+        while (true) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Nhap canh thu 1: ");
+                side1 = Double.parseDouble(input.nextLine());
+                System.out.println("Nhap canh thu 2: ");
+                side2 = Double.parseDouble(input.nextLine());
+                System.out.println("Nhap canh thu 3: ");
+                side3 = Double.parseDouble(input.nextLine());
+                IllegalTriangleException triangle = new IllegalTriangleException();
+                triangle.illegalTriangleException(side1, side2, side3);
+            } catch (NumberFormatException e) {
+                System.err.println("Cac canh phai la so " + e.getMessage());
+            } catch (Exception e) {
+                System.err.println("Cac canh khong dat yeu cau la canh tam giac " + e.getMessage());
+            }
         }
     }
+
+
 }
